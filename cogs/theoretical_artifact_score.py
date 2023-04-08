25996c04d1be1f523fcbdd66197fcc7a23b0ade2
ivixvi <epitus628@gmail.com>
2023-04-09 04:57:13 +0900

    一般的な算出ロジックを追加 おまけでartifact.pyに合わせてシングルクォートに変更

--- cogs/theoretical_artifact_score.py
+++ cogs/theoretical_artifact_score.py
@@ -3,16 +3,16 @@ from decimal import Decimal, ROUND_HALF_UP
 
 class TheoreticalArtifactScore:
     theoretical_value = {
-        "fixed_hp": 299,
-        "fixed_atk": 19,
-        "fixed_def": 23,
-        "rated_hp": 5.8,
-        "rated_atk": 5.8,
-        "rated_def": 7.3,
-        "crit_dmg": 7.8,
-        "crit_rate": 3.9,
-        "charge_rate": 6.5,
-        "elemental_mastery": 23,
+        'fixed_hp': 299,
+        'fixed_atk': 19,
+        'fixed_def': 23,
+        'rated_hp': 5.8,
+        'rated_atk': 5.8,
+        'rated_def': 7.3,
+        'crit_dmg': 7.8,
+        'crit_rate': 3.9,
+        'charge_rate': 6.5,
+        'elemental_mastery': 23,
     }
 
     def __init__(
@@ -39,8 +39,20 @@ class TheoreticalArtifactScore:
         self.charge_rate = charge_rate
         self.elemental_mastery = elemental_mastery
 
+    def _calc_by_general_score_logic(self, calc_type: str) -> float:
+        if calc_type == 'rated_hp':
+            return self.crit_rate * 2 + self.crit_dmg + self.rated_hp
+        if calc_type == 'rated_atk':
+            return self.crit_rate * 2 + self.crit_dmg + self.rated_atk
+        if calc_type == 'crit_only':
+            return self.crit_rate * 2 + self.crit_dmg
+        return 0
+    
+    def calc_general_rate(self, calc_type: str) -> Decimal:
+        return self._quantize(self._calc_by_general_score_logic(calc_type))
+
     def _quantize(self, value: float) -> Decimal:
-        return Decimal(value).quantize(Decimal("0.1"), ROUND_HALF_UP)
+        return Decimal(value).quantize(Decimal('0.1'), ROUND_HALF_UP)
 
     def _calc_theoretical_rate(
         self, attr: str, initial: int = 1, raises: int = 5
@@ -53,7 +65,7 @@ class TheoreticalArtifactScore:
         return getattr(self, attr) / (self.theoretical_value[attr] * (initial + raises))
 
     def calc_theoretical_rate(
-        self, attrs: "list[str] | None" = None, raises: int = 5
+        self, attrs: 'list[str] | None' = None, raises: int = 5
     ) -> Decimal:
         """
         選択したオプションと、上昇回数から想定される理論値から見た割合を算出する
@@ -69,7 +81,7 @@ class TheoreticalArtifactScore:
         return self._quantize(sum(rates) * 100)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # 手持ちのそこそこ強い羽
     tas = TheoreticalArtifactScore(
         fixed_hp=209,
@@ -79,15 +91,15 @@ if __name__ == "__main__":
     )
     # 会心率オプションとしての理論値から見た割合
     # 43.162393162393165
-    print(tas.calc_theoretical_rate(["crit_rate"]))
+    print(tas.calc_theoretical_rate(['crit_rate']))
 
     # 会心率が2回伸びた際の理論値から見た割合
     # 86.32478632478633
-    print(tas.calc_theoretical_rate(["crit_rate"], 2))
+    print(tas.calc_theoretical_rate(['crit_rate'], 2))
 
     # hp固定値を除いた聖遺物の理論値から見た割合
     # 86.07979664014147
-    print(tas.calc_theoretical_rate(["crit_rate", "crit_dmg", "rated_hp"]))
+    print(tas.calc_theoretical_rate(['crit_rate', 'crit_dmg', 'rated_hp']))
 
     # 聖遺物としての理論値から見た割合
     # 84.28200429699679
@@ -102,15 +114,15 @@ if __name__ == "__main__":
     )
     # 会心率オプションとしての理論値から見た割合
     # 11.53846153846154
-    print(tas.calc_theoretical_rate(["crit_rate"]))
+    print(tas.calc_theoretical_rate(['crit_rate']))
 
     # hp固定値が5回伸びた際の理論値から見た割合
     # 69.89966555183946
-    print(tas.calc_theoretical_rate(["fixed_hp"], 5))
+    print(tas.calc_theoretical_rate(['fixed_hp'], 5))
 
     # hp固定値を除いた聖遺物としての理論値から見た割合
     # 26.143899204244036
-    print(tas.calc_theoretical_rate(["crit_rate", "crit_dmg", "rated_hp"]))
+    print(tas.calc_theoretical_rate(['crit_rate', 'crit_dmg', 'rated_hp']))
 
     # 聖遺物としての理論値スコア
     # 69.83879854944321
@@ -125,7 +137,7 @@ if __name__ == "__main__":
     )
     # 率ダメ理論値スコア
     # 100
-    print(tas.calc_theoretical_rate(["crit_rate", "crit_dmg"]))
+    print(tas.calc_theoretical_rate(['crit_rate', 'crit_dmg']))
     # 理論値スコア
     # 100
     print(tas.calc_theoretical_rate())
