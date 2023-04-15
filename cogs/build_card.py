2813611049215856028493b68df996af5a1a264d
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-04-15 14:22:27 +0900

    多分できた

--- cogs/build_card.py
+++ cogs/build_card.py
@@ -1,9 +1,83 @@
+from io import BytesIO
 from discord.ext import commands
 from artifacter_image_gen import Generator
 from enkanetwork import EnkaNetworkAPI
 import discord
+from .build_card_constants import calc_types, prop_id_ja
 
 client = EnkaNetworkAPI(lang='jp')
+
+
+class View(discord.ui.View):
+    def __init__(self, characters):
+        super().__init__(timeout=10)
+        self.characters = characters
+        for i, character in enumerate(characters):
+            self.character.add_option(
+                label=character.name,
+                value=i
+            )
+        for i, calc_type in enumerate(calc_types):
+            tmp = []
+            for r in calc_type['rates']:
+                type_ = r['type']
+                rate = r['rate']
+                if rate == 1:
+                    tmp.append(prop_id_ja[type_])
+                else:
+                    tmp.append(f'{prop_id_ja[type_]} * {rate}')
+            desc = ' + '.join(tmp)
+            self.calc_type.add_option(
+                label=calc_type['label'],
+                description=desc,
+                value=i
+            )
+
+    async def on_timeout(self):
+        await self.message.edit(view=None, content='Timeout')
+
+    @discord.ui.select(
+        cls=discord.ui.Select,
+        placeholder='キャラクター'
+    )
+    async def character(self, interaction: discord.Interaction, select):
+        await interaction.response.defer()
+
+    @discord.ui.select(
+        cls=discord.ui.Select,
+        placeholder='計算タイプ'
+    )
+    async def calc_type(self, interaction: discord.Interaction, select):
+        await interaction.response.defer()
+
+    @discord.ui.button(
+        label='生成',
+        style=discord.ButtonStyle.success
+    )
+    async def generate(self, interaction: discord.Interaction, button):
+        if not self.character.values:
+            await interaction.response.send_message(
+                content='キャラクター選択してない',
+                delete_after=5
+            )
+            return
+        if not self.calc_type.values:
+            await interaction.response.send_message(
+                content='計算タイプ選択してない',
+                delete_after=5
+            )
+            return
+        await self.message.edit(view=None, content='生成中')
+        character = self.characters[int(self.character.values[0])]
+        calc_type = calc_types[int(self.calc_type.values[0])]
+        image = Generator(character).generate(**calc_type)
+        f = BytesIO()
+        image.save(f, format='png')
+        f.seek(0)
+        await self.message.edit(
+            content=None,
+            attachments=[discord.File(f, 'card.png')]
+        )
 
 
 class BuildCard(commands.Cog):
@@ -14,8 +88,9 @@ class BuildCard(commands.Cog):
     async def build(self, ctx, uid: int):
         async with client:
             data = await client.fetch_user(uid)
-            img = Generator(data.characters[0]).generate('RATED_ATK')
-            await ctx.reply('o')
+
+        view = View(data.characters)
+        view.message = await ctx.reply(view=view)
 
 
 async def setup(bot):
---
+++ cogs/build_card_constants.py
@@ -0,0 +1,135 @@
+calc_types = [
+    {
+        'rates': [
+            {'type': 'FIGHT_PROP_CRITICAL', 'rate': 2},
+            {'type': 'FIGHT_PROP_CRITICAL_HURT', 'rate': 1}
+        ],
+        'label': '会心のみ',
+        # TODO
+        'point_refer': {
+            'Total': {
+                'SS': 220,
+                'S': 200,
+                'A': 180
+            },
+            'EQUIP_BRACER': {
+                'SS': 50,
+                'S': 45,
+                'A': 40
+            },
+            'EQUIP_NECKLACE': {
+                'SS': 50,
+                'S': 45,
+                'A': 40
+            },
+            'EQUIP_SHOES': {
+                'SS': 45,
+                'S': 40,
+                'A': 35
+            },
+            'EQUIP_RING': {
+                'SS': 45,
+                'S': 40,
+                'A': 37
+            },
+            'EQUIP_DRESS': {
+                'SS': 40,
+                'S': 35,
+                'A': 30
+            }
+        }
+    },
+    {
+        'rates': [
+            {'type': 'FIGHT_PROP_CRITICAL', 'rate': 2},
+            {'type': 'FIGHT_PROP_CRITICAL_HURT', 'rate': 1},
+            {'type': 'FIGHT_PROP_ATTACK_PERCENT', 'rate': 1}
+        ],
+        'label': '攻撃換算',
+        'point_refer': {
+            'Total': {
+                'SS': 220,
+                'S': 200,
+                'A': 180
+            },
+            'EQUIP_BRACER': {
+                'SS': 50,
+                'S': 45,
+                'A': 40
+            },
+            'EQUIP_NECKLACE': {
+                'SS': 50,
+                'S': 45,
+                'A': 40
+            },
+            'EQUIP_SHOES': {
+                'SS': 45,
+                'S': 40,
+                'A': 35
+            },
+            'EQUIP_RING': {
+                'SS': 45,
+                'S': 40,
+                'A': 37
+            },
+            'EQUIP_DRESS': {
+                'SS': 40,
+                'S': 35,
+                'A': 30
+            }
+        }
+    },
+    {
+        'rates': [
+            {'type': 'FIGHT_PROP_CRITICAL', 'rate': 2},
+            {'type': 'FIGHT_PROP_CRITICAL_HURT', 'rate': 1},
+            {'type': 'FIGHT_PROP_HP_PERCENT', 'rate': 1}
+        ],
+        'label': 'HP換算',
+        'point_refer': {
+            'Total': {
+                'SS': 220,
+                'S': 200,
+                'A': 180
+            },
+            'EQUIP_BRACER': {
+                'SS': 50,
+                'S': 45,
+                'A': 40
+            },
+            'EQUIP_NECKLACE': {
+                'SS': 50,
+                'S': 45,
+                'A': 40
+            },
+            'EQUIP_SHOES': {
+                'SS': 45,
+                'S': 40,
+                'A': 35
+            },
+            'EQUIP_RING': {
+                'SS': 45,
+                'S': 40,
+                'A': 37
+            },
+            'EQUIP_DRESS': {
+                'SS': 40,
+                'S': 35,
+                'A': 30
+            }
+        }
+    }
+]
+
+prop_id_ja = {
+    'FIGHT_PROP_HP': 'HP',
+    'FIGHT_PROP_ATTACK': '攻撃実数',
+    'FIGHT_PROP_DEFENSE': '防御実数',
+    'FIGHT_PROP_HP_PERCENT': 'HP%',
+    'FIGHT_PROP_ATTACK_PERCENT': '攻撃%',
+    'FIGHT_PROP_DEFENSE_PERCENT': '防御%',
+    'FIGHT_PROP_CRITICAL': '会心率',
+    'FIGHT_PROP_CRITICAL_HURT': '会心ダメージ',
+    'FIGHT_PROP_CHARGE_EFFICIENCY': '元素チャ効率',
+    'FIGHT_PROP_ELEMENT_MASTERY': '元素熟知'
+}
